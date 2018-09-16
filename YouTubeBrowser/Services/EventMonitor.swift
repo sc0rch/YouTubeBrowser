377ee76e7ec4d51b4b019c26adb12f31bb7c2e6c
Anton Aleshkevich <sc0rch@me.com>
2018-09-16 11:25:51 +0300

    Add EventMonitor class for future menu bar popover support

---
+++ YouTubeBrowser/Services/EventMonitor.swift
@@ -0,0 +1,35 @@
+//
+//  EventMonitor.swift
+//  YouTubeBrowser
+//
+//  Created by Anton Aleshkevich on 16/09/2018.
+//  Copyright © 2018 Anton Aleshkevich. All rights reserved.
+//
+
+import Cocoa
+
+class EventMonitor {
+  private var monitor: Any?
+  private let mask: NSEvent.EventTypeMask
+  private let handler: (NSEvent?) -> Void
+
+  init(mask: NSEvent.EventTypeMask, handler: @escaping (NSEvent?) -> Void) {
+    self.mask = mask
+    self.handler = handler
+  }
+
+  deinit {
+    stop()
+  }
+
+  func start() {
+    monitor = NSEvent.addGlobalMonitorForEvents(matching: mask, handler: handler)
+  }
+
+  func stop() {
+    if monitor != nil {
+      NSEvent.removeMonitor(monitor!)
+      monitor = nil
+    }
+  }
+}
